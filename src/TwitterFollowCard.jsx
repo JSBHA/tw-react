beee56ce17aec3278ad229055765a5e32d1bc1e6
JSBHA <jhoan-071995@hotmail.com>
2025-04-01 10:56:03 -0500

    For the follow button, we are going to use the state changes that would be the hooks, where the button when clicked is 'follow' or 'following'

--- src/TwitterFollowCard.jsx
+++ src/TwitterFollowCard.jsx
@@ -1,23 +1,36 @@
-export function TwitterFollowCard({UserName, children, isFollowing }){
-    console.log(isFollowing)
+import { useState } from "react";
 
-    return (
-        <article className='tw-followCard'>
-            <header className='tw-followCard-header'>
-                <img 
-                className='tw-followCard-avatar'
-                alt="Mi avatar"
-                src={`https://unavatar.io/${UserName}`} />
-                <div className='tw-followCard-info'>
-                    <strong>{children}</strong>
-                    <span className='tw-followCard-infoUserName'>@{UserName}</span>
-                </div>
-            </header>
-            <aside>
-                <button className='tw-followCard-button'>
-                    seguir
-                </button>
-            </aside>
-        </article>
-    )
+export function TwitterFollowCard({ UserName, children }) {
+    
+    const [isFollowing, setIsFollowing] = useState(false)
+
+  const text = isFollowing ? "Siguiendo" : "Seguir";
+  const buttonClassName = isFollowing
+    ? "tw-followCard-button is-following"
+    : "tw-followCard-button";
+
+    const handleClick = () =>{
+        setIsFollowing(!isFollowing )
+    }
+ 
+  return (
+    <article className="tw-followCard">
+      <header className="tw-followCard-header">
+        <img
+          className="tw-followCard-avatar"
+          alt="Mi avatar"
+          src={`https://unavatar.io/${UserName}`}
+        />
+        <div className="tw-followCard-info">
+          <strong>{children}</strong>
+          <span className="tw-followCard-infoUserName">@{UserName}</span>
+        </div>
+      </header>
+      <aside>
+        <button 
+        className={buttonClassName} onClick={handleClick}>
+        {text}</button>
+      </aside>
+    </article>
+  );
 }
